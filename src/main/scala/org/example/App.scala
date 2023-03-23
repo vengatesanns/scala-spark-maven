91b60f88c9015e9d4b20624f6ce49184ae213729
vengatesanns <vengat19amd@gmail.com>
2023-03-23 09:51:04 +0530

    Initial Commit

---
+++ src/main/scala/org/example/App.scala
@@ -0,0 +1,30 @@
+package org.example
+
+import org.apache.log4j.lf5.LogLevel
+import org.apache.spark.sql.SparkSession
+
+object App {
+
+  def main(args: Array[String]): Unit = {
+    val sparkSession = SparkSession.builder().appName("test").master("local[*]").getOrCreate()
+    sparkSession.sparkContext.setLogLevel("ERROR")
+    val url = "jdbc:mysql://localhost:3306/employee"
+    val driver = "com.mysql.cj.jdbc.Driver"
+    val userName = "root"
+    val password = "12345"
+    val query = "(select * from user_info where active = 1) as users"
+
+    val df = sparkSession.read
+      .format("jdbc")
+      .option("url", url)
+      .option("driver", driver)
+      .option("dbtable", query)
+      .option("user", userName)
+      .option("password", password)
+      .load()
+
+    df.show()
+
+  }
+
+}
